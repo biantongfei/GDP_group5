f9658be53e0a61b66fc2f22c6fdb885ea8134972
biantongfei <tongfeibian@gmail.com>
2022-02-04 14:16:31 +0000

    pose detection

--- pose_detection/Alphapose/detector/yolo_api.py
+++ pose_detection/Alphapose/detector/yolo_api.py
@@ -18,7 +18,7 @@ from yolo.darknet import Darknet
 from yolo.util import unique
 from yolo.bbox import bbox_iou
 
-from detector.apis import BaseDetector
+from apis import BaseDetector
 
 #only windows visual studio 2013 ~2017 support compile c/cuda extensions
 #If you force to compile extension on Windows and ensure appropriate visual studio
