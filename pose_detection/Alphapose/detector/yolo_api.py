324d7c2745b55bf6dc6c21443ad09a83bb35e135
biantongfei <tongfeibian@gmail.com>
2022-02-04 19:28:18 +0000

    pose detection

--- pose_detection/Alphapose/detector/yolo_api.py
+++ pose_detection/Alphapose/detector/yolo_api.py
@@ -33,8 +33,8 @@ class YOLODetector(BaseDetector):
 
         self.detector_cfg = cfg
         self.detector_opt = opt
-        self.model_cfg = cfg.get('CONFIG', 'detector/yolo/cfg/yolov3-spp.cfg')
-        self.model_weights = cfg.get('WEIGHTS', 'detector/yolo/data/yolov3-spp.weights')
+        self.model_cfg = cfg.get('CONFIG', 'yolo/cfg/yolov3-spp.cfg')
+        self.model_weights = cfg.get('WEIGHTS', 'yolo/data/yolov3-spp.weights')
         self.inp_dim = cfg.get('INP_DIM', 608)
         self.nms_thres = cfg.get('NMS_THRES', 0.6)
         self.confidence = 0.3 if (False if not hasattr(opt, 'tracking') else opt.tracking) else cfg.get('CONFIDENCE', 0.05)
