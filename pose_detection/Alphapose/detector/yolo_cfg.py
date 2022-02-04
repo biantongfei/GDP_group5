dd06c845d17b475892d4d228835064af365d0673
biantongfei <tongfeibian@gmail.com>
2022-02-04 20:32:38 +0000

    pose detection

--- pose_detection/Alphapose/detector/yolo_cfg.py
+++ pose_detection/Alphapose/detector/yolo_cfg.py
@@ -1,8 +1,8 @@
 from easydict import EasyDict as edict
 
 cfg = edict()
-cfg.CONFIG = 'yolo/cfg/yolov3-spp.cfg'
-cfg.WEIGHTS = 'yolo/data/yolov3-spp.weights'
+cfg.CONFIG = 'Alphapose/detector/yolo/cfg/yolov3-spp.cfg'
+cfg.WEIGHTS = 'Alphapose/detector/yolo/data/yolov3-spp.weights'
 cfg.INP_DIM =  608
 cfg.NMS_THRES =  0.6
 cfg.CONFIDENCE = 0.1
